1a56a81189229285c1add2092324e5efabcd7bb6
Jérémy Bonvoisin <jeremy.bonvoisin@gmail.com>
2019-06-12 22:41:01 +0100

    attempt create committer graph with recursive function

--- goCreateGraphs.py
+++ goCreateGraphs.py
@@ -51,111 +51,206 @@ def help():
 
 def getCommitterGraph(commits):
     
-    committers, stats = getCommitters(commits)
-    numberOfCommitters = len(committers)
+    users = getUsers(commits)
+    numberOfUsers = len(users)
 
     # create adjacency matrix
-    adjacencyMatrix = numpy.zeros([numberOfCommitters, numberOfCommitters])
+    adjacencyMatrix = numpy.zeros([numberOfUsers, numberOfUsers])
     
-    
+       
     # fill adjacency matrix
     for commit in commits:
+        # GitHub Issue #13 Complex interaction cases in commits 
+        currentCommitter = matchUser(fetchUserInfo(commit, "author"), users)
+        print ("    current commit :" + commit['url'])
         if "files" in commit.keys():
                 for file in commit['files']:
+                    print ("        looking for interactions in file" + file["filename"])
                     if not file['status']=='added':
-                        # recursively look for commit parents and check if the file has been edited
-                        exitCondition = False
-                        referenceCommit = commit
-                        while not exitCondition:
-                            if not referenceCommit['parents']: # there is no parent, we stop
-                                exitCondition = True
+                        lastEdits = []
+                        for parent in commit['parents']:
+                            parentFullDetails = [ x for x in commits if x["sha"] == parent['sha']]
+                            if len(parentFullDetails)==1:
+                                lastEdits += searchLastEdit(commits, parentFullDetails[0], file["filename"])
                             else:
-                                for parent in referenceCommit['parents']:
-                                    referenceCommit = [ x for x in commits if x["sha"] == parent['sha']]
-                                    if any(x['filename'] == file['filename'] for x in referenceCommit['files']): # is the file affected by the referenceCommit?
-                                        exitCondition = True                               
-                        
-                                
-                        
-                
-                
-        else:
-            #print("commit " + sha + " has no attribute 'files'.")
-            stats["noFiles"] += 1        
+                                print ("error: no commit or more than one commit corresponding to sha: " + parent['sha'] + ": "+ str(parentFullDetails))
+                        #print("     edits found " + str(len(lastEdits)) + " " + str([x['sha'] for x in lastEdits]))
+                        # the list of returned commits should contain:
+                        #     - at least one item, since you would expect that a file which is "modified" in the current commit has at least been "added" in a previous commit, if not "modified"
+                        #     - max 2 different items, since a commit can have max 2 parents
+                        #     - nonetheless, the list of returned items can contain duplicates, since the recursive search can find a commit following different branches    
+                        lastEdits = [element for index, element in enumerate(lastEdits) if element not in lastEdits[index + 1:]] # removes the duplicates
+                        #print("edits found " + str(len(lastEdits)) + " " + str([x['sha'] for x in lastEdits]))
+                        if len(lastEdits)==0 or len(lastEdits)>2:
+                            print ("error - returned list of commits should contain 1 or 2 items: " + str(lastEdits))
+                            print ("    current commit : " + commit['url'])
+                            print ("    current file :" + file["filename"])
+                        for lastEdit in lastEdits:
+                            lastAuthoringUser = matchUser(fetchUserInfo(lastEdit, "author"), users)
+                            lastCommittingUser = matchUser(fetchUserInfo(lastEdit, "committer"), users)
+                            #print(lastAuthoringUser)
+                            #print (lastAuthoringUser['uniqueID'])
+                            #print(lastCommittingUser)
+                            #print (lastCommittingUser['uniqueID'])
+                            adjacencyMatrix[lastCommittingUser['uniqueID'],currentCommitter['uniqueID']] += 1
+                            if lastAuthoringUser!=lastCommittingUser:
+                                adjacencyMatrix[lastAuthoringUser['uniqueID'],currentCommitter['uniqueID']] += 1
 
     # transform the adjacency matrix in a graph
-    G = nx.MultiDiGraph()
+    G = nx.DiGraph()
 
+    # create nodes
+    for i in range(0,numberOfUsers):
+        user = [x for x in users if x['uniqueID']==i][0]
+        G.add_node(
+            i, # corresponds to uniqueID of the user
+            logins = user['logins'],
+            names = user['names'],
+            emails = user['emails']
+        )
+    #print(G.nodes(data=True))
+
+    # create nodes
+    for i in range(0,numberOfUsers):
+        for j in range(0,numberOfUsers):
+            if adjacencyMatrix[i,j]!=0:
+                G.add_edge(i, j, weight=adjacencyMatrix[i,j])
+    #print(G.edges(data=True))
+        
     # return graph
-    return G, stats
-  
+    return G
+ 
 ###################################################################################################################
 # FUNCTION
 ###################################################################################################################
-# Creates and returns a list of people identified by login, name and email
+# Creates and returns a list of users without duplicates where users are identified by a list of possible logins, names and emails
 
-def getCommitters(commits):
+# TODO issue #12 - check the quality of the extracted commits during extraction
+# move the following to gomine.
+#print("commit " + commit['sha'] + " has no attribute 'committer'.")
+#print("invalid commit format: " + str(commit))
+#print("commit " + commit['sha'] + " has no attribute 'author'.")
+#stats = {"commits" : 0, "noAuthor" : 0, "noCommitter" : 0, "noFiles": 0, "empty": 0}
 
-    stats = {"commits" : 0, "noAuthor" : 0, "noCommitter" : 0, "noFiles": 0, "empty": 0}
+def getUsers(commits):
 
     # fetch author and committer infos from commits and put them in a list of dicts {'login':, 'name':, 'email':}
-    committers = []
-
-    stats["commits"] += 1
+    users = []
     for commit in commits:
-
         if "commit" in commit.keys():
-            author = {}
-            author['email'] = commit['commit']['author']['email']
-            author['name'] = commit['commit']['author']['name']
-            try:
-                author['login'] = commit['author']['login']
-            except TypeError as err:
-                #print("commit " + commit['sha'] + " has no attribute 'author'.")
-                author['login'] = ""
-                stats["noAuthor"] += 1
-            
-            committer = {}
-            committer['email'] = commit['commit']['committer']['email']
-            committer['name'] = commit['commit']['committer']['name']
-            try:
-                committer['login'] = commit['committer']['login']
-            except TypeError as err:
-                #print("commit " + commit['sha'] + " has no attribute 'committer'.")
-                committer['login'] = ""
-                stats["noCommitter"] += 1
+            author = fetchUserInfo(commit, 'author')
+            committer = fetchUserInfo(commit, 'committer')
+            if not committer in users:
+                users.append(committer)
+            if not author in users:
+                users.append(author)
 
-            if not committer in committers:
-                committers.append(committer)
-            if not author in committers:
-                committers.append(author)
-        else :
-            #print("invalid commit format: " + str(commit))
-            stats["empty"] += 1
-            
-    # merge similar profiles into a list of dicts {'uniqueID', 'login(s)':, 'name(s)':, 'email(s)':} 
-    uniqueCommitters = []
-    for committer in committers:
-        toBeMerged = [x for x in committers if \
-            x['login']!= "" and x['login']==committer['login'] or \
-            x['name']!= "" and x['name']==committer['name'] or \
-            x['name']!= "" and x['email']==committer['email'] \
-        ]
-        logins = list(set(map(lambda x : x['login'], toBeMerged))) # converting to a set removes the duplicates
-        names = list(set(map(lambda x : x['name'], toBeMerged)))
-        emails = list(set(map(lambda x : x['email'], toBeMerged)))
-
-        uniqueCommitter = { \
-            'uniqueID': len(uniqueCommitters), \
-            'logins': logins, \
-            'names': names, \
-            'emails': emails \
-        }
-        
-        uniqueCommitters.append(uniqueCommitter)
-
-    return uniqueCommitters, stats
+    print ("    " + str(len(users)) + " users found: " + str(users))
+    # merge similar profiles into a list of dicts {'uniqueID':, 'login(s)':, 'name(s)':, 'email(s)':}
+    uniqueUsers = []
+    for user in users:
     
+        # fetch users having something in common with the tested user in the list of unmerged users
+        usersToBeMerged = [x for x in users if \
+            user['login'] != "" and user['login'] == x['login'] or \
+            user['name'] != "" and user['name'] == x['name'] or \
+            user['email'] != "" and user['email'] == x['email'] \
+        ]
+        # fetch users having something in common with the tested user in the list of merged users
+        uniqueUsersToBeMerged = [x for x in uniqueUsers if \
+            user['login'] != "" and user['login'] in x['logins'] or \
+            user['name'] != "" and user['name'] in x['names'] or \
+            user['email'] != "" and user['email'] in x['emails'] \
+        ]
+        # compile all the infos of these duplicate users
+        logins = list(map(lambda x : x['login'], usersToBeMerged)) + [item for sublist in list(map(lambda x : x['logins'], uniqueUsersToBeMerged)) for item in sublist]
+        names = list(map(lambda x : x['name'], usersToBeMerged)) + [item for sublist in list(map(lambda x : x['names'], uniqueUsersToBeMerged)) for item in sublist]
+        emails = list(map(lambda x : x['email'], usersToBeMerged)) + [item for sublist in list(map(lambda x : x['emails'], uniqueUsersToBeMerged)) for item in sublist]
+                
+        uniqueUser = { \
+            'uniqueID': None, \
+            'logins': list(set(logins)), \
+            'names': list(set(names)), \
+            'emails': list(set(emails)) \
+        } # converting to a set removes the duplicates
+
+        # replace the merged users in the list of unique users with merged versions of these same users
+        for userToRemove in uniqueUsersToBeMerged:
+            uniqueUsers.remove(userToRemove)
+        uniqueUsers.append(uniqueUser)
+ 
+    for i, uniqueUser in enumerate(uniqueUsers):
+        uniqueUser['uniqueID'] = i
+
+    print ("    " + str(len(uniqueUsers)) + " unique users identified: " + str(uniqueUsers))
+ 
+    return uniqueUsers
+
+###################################################################################################################
+# FUNCTION
+###################################################################################################################
+# fetches user info from a commit and put them in a dict {'login':, 'name':, 'email':}
+# all strings are lowercased
+# role is either "author" or "committer"
+
+def fetchUserInfo(commit, role):
+
+    user = {}
+    user['email'] = commit['commit'][role]['email'].lower()
+    user['name'] = commit['commit'][role]['name'].lower()
+    try:
+        user['login'] = commit[role]['login'].lower()
+    except TypeError as err:
+        user['login'] = ""
+    return user
+
+###################################################################################################################
+# FUNCTION
+###################################################################################################################
+# match a user from a list of users and returns the found user dict
+
+def matchUser(user, userList):
+
+    foundUser = [x for x in userList if \
+        user['login'] != "" and user['login'] in x['logins'] or \
+        user['name'] != "" and user['name'] in x['names'] or \
+        user['email'] != "" and user['email'] in x['emails'] \
+    ]
+    if len(foundUser)!=1:
+        raise Exception("there is either no user or more than one user that fit with the following data: "+ str(user) + " returned users " +str(foundUser))
+    return foundUser[0]
+
+###################################################################################################################
+# FUNCTION
+###################################################################################################################
+# searches the last commit in the history of the given commit where the file has been edited
+def searchLastEdit(commits, commit, fileName):
+
+    #print (commit['url'])
+    lookedForCommit = []
+    fileFound = False
+        
+    try:
+        for file in commit['files']:
+            if file['filename'].split("/")[-1]==fileName.split("/")[-1]: # we only look at the filename and not the path
+                fileFound = True
+    except KeyError as err:
+        pass   
+        
+    if fileFound: # is the file affected by the current commit?
+        #print("found commit : " + commit['sha'])
+        lookedForCommit+= [commit]
+
+    else:
+        for parent in commit['parents']:
+            parentFullDetails = [ x for x in commits if x["sha"] == parent['sha']]
+            if len(parentFullDetails)==1:
+                lookedForCommit+= searchLastEdit(commits, parentFullDetails[0], fileName)
+            else:
+                print ("error: no commit or more than one commit corresponding to sha: " + parent['sha'] + ": "+ str(parentFullDetails))
+
+    return lookedForCommit
+   
 ###################################################################################################################
 # FUNCTION
 ###################################################################################################################
@@ -163,11 +258,9 @@ def getCommitters(commits):
 def getCommitGraph(commits):
 
     G = nx.DiGraph()
-    stats = {"commits" : 0, "noAuthor" : 0, "noCommitter" : 0, "noFiles": 0, "empty": 0}
     
     for commit in commits:
         
-        stats["commits"] += 1
         if 'sha' in commit.keys():
 
             sha = commit['sha']
@@ -189,25 +282,17 @@ def getCommitGraph(commits):
             try:
                 G.nodes[sha]['committerLogin'] = commit['committer']['login']
             except TypeError as err:
-                #print("commit " + sha + " has no attribute 'committer'.")
-                stats["noCommitter"] += 1
+                pass
      
             try:
                 G.nodes[sha]['authorLogin'] = commit['author']['login']
             except TypeError as err:
-                #print("commit " + sha + " has no attribute 'author'.")
-                stats["noAuthor"] += 1
-     
+                pass
+                
             try:
                 G.nodes[sha]['affectedFiles'] = len(commit['files'])
             except KeyError as err:
-                #print("commit " + sha + " has no attribute 'files'.")
-                stats["noFiles"] += 1
-
-        else :
-            #print("invalid commit format: " + str(commit))
-            stats["empty"] += 1
-            
+                pass            
   
     for commit in commits:
         try:
@@ -216,7 +301,7 @@ def getCommitGraph(commits):
         except Exception as err:
             pass
 
-    return G, stats
+    return G
 
 
 ###################################################################################################################
@@ -288,7 +373,6 @@ else:
 if not os.path.exists(outputDir):
     os.makedirs(outputDir)
   
-stats = {"commits" : 0, "noAuthor" : 0, "noCommitter" : 0, "noFiles": 0, "empty": 0}  
 #build graphs for all variations for all JSON files
 for fileToProcess in filesToProcess:
 
@@ -298,26 +382,21 @@ for fileToProcess in filesToProcess:
         with open(os.path.join(inputDir,fileToProcess)) as json_file:
             commits = json.load(json_file)
 
-        outputGraphFileName = os.path.join(outputDir, fileNameRoot+".graphml")
+        outputGraphFileName = os.path.join(outputDir, fileNameRoot+".gml")
         
         if rewrite or not os.path.exists(outputGraphFileName):
+            print("Creating graph for "+fileNameRoot)
             if graphType == "commit":
-                graph, localStats = getCommitGraph(commits)
+                graph = getCommitGraph(commits)
             if graphType == "committer":
-                graph, localStats = getCommitterGraph(commits)
+                graph = getCommitterGraph(commits)
             
-            print("Graph created for "+fileNameRoot)
-            print("\t with "+str(localStats))
-            stats["commits"] += localStats["commits"]
-            stats["noAuthor"] += localStats["noAuthor"]
-            stats["noCommitter"] += localStats["noCommitter"]
-            stats["noFiles"] += localStats["noFiles"]
-            stats["empty"] += localStats["empty"]
-            nx.write_graphml(graph, outputGraphFileName)                
+            nx.write_gml(graph, outputGraphFileName)                
+            print("    Graph created with "+str(len(graph.nodes()))+ " nodes")
         else:
             print("Graph for "+fileNameRoot + "already exists")            
             
     except json.decoder.JSONDecodeError as err:
         print("error while decoding json from file '" + fileToProcess + "'. Error returned: " + str(err))
     
-print("graphs created with "+str(stats))
+print("termniated withour errors")
